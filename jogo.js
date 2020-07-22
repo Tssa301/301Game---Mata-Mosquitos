943826beab4e016bc8949ea75009a10fbac31d92
Tiago Ssa <tiago.silva301@gmail.com>
2020-07-22 23:10:29 +0100

    Criado novo arquivo (vitoria.html) para o fluxo de Vitória, index para pagina inicial e a implementação da lógica para o nível do jogo.

--- jogo.js
+++ jogo.js
@@ -4,6 +4,22 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+    //1500
+    criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+    //1000
+    criaMosquitoTempo = 1000
+} else if (nivel === 'modohard') {
+    //750
+    criaMosquitoTempo = 750
+}
+
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
@@ -16,10 +32,11 @@ ajustaTamanhoPalcoJogo()
 var cronometro = setInterval(function() {
 
     tempo -= 1
+
     if(tempo < 0) {
         clearInterval(cronometro)
         clearInterval(criaMosquito)
-        alert('vitoria')
+        window.location.href= 'vitoria.html'
     } else {
         document.getElementById('cronometro').innerHTML = tempo
     }
